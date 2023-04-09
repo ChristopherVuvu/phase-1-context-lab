878bff8a827d1950c5e44fb6b4ca83b5a8e8efc0
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-09 03:34:51 +0300

    added

--- index.js
+++ index.js
@@ -1,4 +1,114 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    return{
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents:[],
+        timeOutEvents:[],
+    }
+}
+
+function createEmployeeRecords(arrays){
+    return arrays.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(employeeRecord, dateTimeString){
+    const [date, time] = dateTimeString.split(' ');
+    const [hour, minute] = time.split(':');
+
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(hour),
+        minute: parseInt(minute)
+    })
+    return employeeRecord
+}
+
+function createTimeOutEvent(employeeRecord, dateTimeString) {
+    const [date, time] = this.dateTimeString.split(' ');
+    const [hour, minute] = this.time.split(':');
+    
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        date: this.date,
+        hour: parseInt(hour),
+        minute: parseInt(minute)
+    })
+
+    return employeeRecord
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvent = this.employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.employeeRecord.timeOutEvents.find(event => event.date === date);
+    
+    const timeIn = new Date(timeInEvent.date + ' ' + timeInEvent.hour);
+    const timeOut = new Date(timeOutEvent.date + ' ' + timeOutEvent.hour);
+    
+    const hoursWorked = (timeOut - timeIn) / (1000 * 60 * 60);
+    
+    return hoursWorked;
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+    // find the timeIn and timeOut events for the given date
+    const timeIn = this.employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOut = this.employeeRecord.timeOutEvents.find(event => event.date === date);
+    
+    // calculate the number of hours worked
+    const hoursWorked = (timeOut.hour - timeIn.hour) / 100;
+    
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const payRate = this.employeeRecord.payPerHour;
+    return hoursWorked * payRate;
+}
+
+function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const payRate = this.employeeRecord.payPerHour;
+    const wagesEarned = hoursWorked * payRate;
+    return wagesEarned;
+}
+
+//function allWagesFor(employee) {
+//    let totalWages = 0;
+//    employee.timeInEvents.forEach((timeInEvent) => {
+//        const wages = wagesEarnedOnDate(employee, timeInEvent.date);
+//        totalWages += wages;
+//    });
+//   return totalWages;
+//}
+
+function calculateTotalWages(employees) {
+    let totalWages = 0;
+    
+    for (let i = 0; i < employees.length; i++) {
+        totalWages += allWagesFor(employees[i]);
+    }
+    
+    return totalWages;
+}
+
+function calculatePayroll(employees) {
+    let totalPayroll = 0;
+    
+    employees.forEach(employee => {
+        const wages = allWagesFor(employee);
+        totalPayroll += wages;
+    });
+    
+    return totalPayroll;
+}
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(employee => employee.firstName === firstNameString);
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
